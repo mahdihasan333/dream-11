dd9b98905acf973d05bbc8507e4b341456194e58
popyakter121 <popy2000akter@gmail.com>
2024-10-24 20:22:44 +0500

    bpl dream 11 project setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Banner from "./components/Banner";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      {/* Header Section */}
+      <Header></Header>
+      {/* Banner Section */}
+      <Banner></Banner>
+      {/* Main Section */}
+      {/* Footer Section */}
+    </>
+  );
+}
+
+export default App;
